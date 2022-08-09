486cfc1fa0d6939c5064376d37a8b3a29f422d0d
rebaz36 <rebaz415@gmail.com>
2022-08-10 01:14:08 +0300

    Setup webpack server

--- webpack.config.js
+++ webpack.config.js
@@ -4,9 +4,10 @@ const path = require('path');
 module.exports = {
   entry: './src/index.js',
   output: {
-    filename: 'main.js',
+    filename: '[name].js',
     path: path.resolve(__dirname, 'dist'),
   },
+  mode: 'development',
   module: {
     rules: [
       {

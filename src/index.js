f950e19796dd853a9ea906471f9d638222196ca6
rebaz36 <rebaz415@gmail.com>
2022-09-06 07:15:42 +0300

    Fix ESlint Error

--- src/index.js
+++ src/index.js
@@ -9,11 +9,10 @@ function component() {
   element.innerHTML = _.join(['Hello', 'webpack'], ' ');
   element.classList.add('hello');
 
-
   // Add the image to our existing div.
   const myIcon = new Image();
   myIcon.src = Icon;
-  
+
   element.appendChild(myIcon);
 
   return element;
